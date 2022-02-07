0dba556d08432a3ae67e64a7f1cb6fd088619ecd
indivari <indivarij@gmail.com>
2022-02-07 22:26:13 +1100

    added google maps

--- bookshelf/src/App.js
+++ bookshelf/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import GoogleMapsComponent from "./GoogleMapsComponent";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <div className="App">est</div>
+      <GoogleMapsComponent />
+    </>
+  );
 }
 
 export default App;
