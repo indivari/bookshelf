b3f317ee1fa50424c38c0ffe3c95d7989d83c963
chando <dave@staticum.net>
2022-02-08 20:55:09 +0800

    finish router controller model to mongodb working - just need to do CRUD methods next

--- bookshelf/routes/users.js
+++ bookshelf/routes/users.js
@@ -1,14 +1,11 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
 
-router.get("/", function (req, res, next) {
-  const t = req.query.t;
-  const u = req.query.f;
+router.get("/add", userController.create_user_post);
 
-  res.send("respond with an user resource '/''" + t + " " + u);
-});
+router.get("/list", userController.user_list_get);
 
-router.get("/:userid", function (req, res, next) {
-  res.send("respond with an id ");
-});
+router.get("/details", userController.user_details_get);
+
 module.exports = router;
