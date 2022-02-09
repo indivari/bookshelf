dcb417ba5f4a4221eb80d6295b15e53a16290aef
SirvanD <sirvansh@gmail.com>
2022-02-09 13:08:54 +1100

    wednesday

--- bookshelf/routes/users.js
+++ bookshelf/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.get("/add", userController.create_user_post);
+router.post("/add", userController.create_user_post);
 
 router.get("/list", userController.user_list_get);
 
